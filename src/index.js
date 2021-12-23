8f032aee218ac24ba5fb892a8470a9bc8fc7e334
Daniel Semple <dsemple.isu@gmail.com>
2021-12-23 10:36:27 -0600

    added remove buttno

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import project from "./project";
 import './style.css';
 
 const projects = [];
-const defaultProject = project('Default');
-const secondProject = project('secondtemp');
+
+const content = document.createElement('div');
+content.setAttribute('id', 'content');
+document.body.appendChild(content);
+
+
+const defaultProject = project('Project Title 1');
+const secondProject = project('Project Title 2');
 
 projects.push(defaultProject);
 projects.push(secondProject);
 
-const defaultTodo = todo('default', 'nothing to do');
+const defaultTodo = todo('todo title', 'nothing to do');
 
 defaultProject.addItem(defaultTodo);
 
@@ -20,29 +26,38 @@ const addTodo = (project) => {
 }
 
 const renderPage = () => {
-const content = document.querySelector('#content');
-content.innerHTML = '';
 
-for (const project of projects) {
+  const content = document.querySelector('#content');
+  content.innerHTML = '';
+
+  for (const project of projects) {
     const projectTab = document.createElement('div');
     projectTab.classList.add('project-tab');
     content.appendChild(projectTab);
     const h2 = document.createElement('h2');
     h2.textContent = project.getTitle();
     const projectList = document.createElement('div');
+    projectList.classList.add('project-list');
 
     for (const todo of project.getList()) {
       const h3 = document.createElement('h3');
       const p = document.createElement('p');
-      const btn = document.createElement('button');
+      const finishedBtn = document.createElement('button');
+      const removeBtn = document.createElement('button');
       h3.textContent = todo.getTitle();
       p.textContent = `${todo.getDescription()}`; 
-      btn.textContent = `${todo.getComplete() ? 'is finished' : 'not finished'}`;
-      btn.onclick = () => {
+      finishedBtn.textContent = `${todo.getComplete() ? 'finished' : 'not finished'}`;
+      finishedBtn.onclick = () => {
         todo.setComplete();
         renderPage();
       }
-      projectList.append(h3, p, btn);
+      removeBtn.textContent = 'Remove';
+      removeBtn.onclick = () => {
+        project.removeItem(todo);
+        renderPage();
+      }
+
+      projectList.append(h3, p, finishedBtn, removeBtn);
     }
 
     const btn = document.createElement('button');
@@ -53,7 +68,16 @@ for (const project of projects) {
 
     projectTab.append(h2, projectList, btn);
   }
+  const addProjectBtn = document.createElement('button');
+  addProjectBtn.textContent = 'Add Project';
+  addProjectBtn.classList.add('add-project-button')
+  addProjectBtn.onclick = () => {
+    projects.push(project('new project'));
+    renderPage();
+  }
+  content.appendChild(addProjectBtn);
 }
+
 
 renderPage();
 
--- src/project.js
+++ src/project.js
@@ -2,8 +2,11 @@ const project = (title) => {
   const list = [];
   const getTitle = () => title;
   const addItem = (item) => list.push(item);
+  const removeItem = (item) => {
+    list.splice(list.indexOf(item), 1);
+  };
   const getList = () => list;
-  return { getTitle, getList, addItem };
+  return { getTitle, getList, addItem,removeItem };
 }
 
 export default project;
